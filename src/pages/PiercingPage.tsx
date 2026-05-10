325afe68d756d87a336e7847497d3e27cbda8d5a
Toska <vickeyteliatnyk@gmail.com>
2026-05-10 23:07:36 +0300

    UI Redesig

--- src/pages/PiercingPage.tsx
+++ src/pages/PiercingPage.tsx
@@ -1,5 +1,5 @@
 import { useBooking } from '../context/BookingContext';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReviewsSection } from './HomePage';
 import { FAQItem } from '../components/FAQItem';
